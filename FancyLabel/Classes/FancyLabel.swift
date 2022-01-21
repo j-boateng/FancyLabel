25901df98c4534905c530924b22897138c7cf141
Jonas Boateng <jonas.boateng@bbc.co.uk>
2022-01-21 15:27:26 +0000

    Removes sample file

--- FancyLabel/Classes/FancyLabel.swift
+++ FancyLabel/Classes/FancyLabel.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class FancyLabel: UILabel{
     public func startBlinking() {
-        let options : UIViewAnimationOptions = [.repeat , .autoreverse]
+        let options : UIView.AnimationOptions = [.repeat , .autoreverse]
         UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
             self.alpha = 0
             }, completion: nil)
