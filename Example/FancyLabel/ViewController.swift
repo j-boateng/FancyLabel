600eac9f72e45a915414e34e7c0a83856f66699a
Jonas Boateng <jonas.boateng@bbc.co.uk>
2022-01-21 15:25:45 +0000

    FancyLabel set to right dir

--- Example/FancyLabel/ViewController.swift
+++ Example/FancyLabel/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import FancyLabel
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var fancyLabel: FancyLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
