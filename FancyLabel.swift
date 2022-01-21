a1d76dee9b5126f2520e70903d800434d274ffbb
Jonas Boateng <jonas.boateng@bbc.co.uk>
2022-01-21 15:14:57 +0000

    Adds Blinking and stop blinking public func

--- FancyLabel.swift
+++ FancyLabel.swift
@@ -8,5 +8,15 @@
 import Foundation
 
 class FancyLabel: UILabel{
-    
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = [.repeat , .autoreverse]
+        UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+            }, completion: nil)
+    }
+
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
 }
